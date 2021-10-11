fbe3c2bc5745864d0613f6629fd6a634ad58aa33
Albert <89455222+amiralberto@users.noreply.github.com>
2021-10-12 00:05:52 +0330

    Add files via upload

---
+++ AlbertCrowdSale.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.9;
+
+import "./Cappedcrowdsale.sol";
+
+contract AlbertCrowdSale is Cappedcrowdsale {
+    uint256 internal constant ethCap = 10000 ether;
+    uint256 internal constant oneEthToTokens = 100000;
+    
+    constructor(address _wallet, ERC20 _token) public 
+        Crowdsale(oneEthToTokens, _wallet, _token)
+        Cappedcrowdsale(ethCap)
+    {
+        
+    }
+    
+}
