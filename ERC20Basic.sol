9a35711e97addf2dd04cc795f5d6cb6efb7e8e2b
Albert <89455222+amiralberto@users.noreply.github.com>
2021-10-12 00:37:44 +0330

    Add files via upload

---
+++ ERC20Basic.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.9;
+
+
+ //title ERC20Basic//
+ 
+ 
+contract ERC20Basic {
+    
+     function totalSupply() public view returns (uint256);
+  
+     function balanceOf(address _who) public view returns (uint256);
+  
+     function transfer(address _to, uint256 _value) public returns (bool);
+  
+    event Transfer(address indexed from, address indexed to, uint256 value);
+  
+}
